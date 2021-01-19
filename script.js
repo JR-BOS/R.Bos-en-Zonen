a119372359727ea0de104e6f31cda3643e79f543
Jesse-Bos <76525071+Jesse-Bos@users.noreply.github.com>
2021-01-19 21:53:36 +0100

    Form submission

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 $(document).ready(function () {
+  $(".submit").click(function (event) {
+    console.log("test");
+
+    var email = $(".email1").val();
+    var subject = $(".subject").val();
+    var name = $(".name1").val();
+    var message = $(".message").val();
+    var statusElm = $(".status");
+    statusElm.empty();
+
+    if (email.length > 5 && email.includes("@") && email.includes(".")) {
+    } else {
+      statusElm.append("<div>email is onjuist!</div>");
+      event.preventDefault();
+    }
+
+    if (subject.length > 2) {
+    } else {
+      statusElm.append("<div>Onderwerp is te kort!</div>");
+      event.preventDefault();
+    }
+
+    if (name.length > 1) {
+    } else {
+      statusElm.append("<div>Naam is te kort!</div>");
+      event.preventDefault();
+    }
+
+    if (message.length > 10) {
+    } else {
+      statusElm.append("<div>Bericht is te kort!</div>");
+      event.preventDefault();
+    }
+  });
+
   $(window).scroll(function () {
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
